f1e945e8b71810d75cc149302e24aa7c3308bf51
meguerhi1 <meguerhi1@gmail.com>
2025-02-24 21:30:27 +0100

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/screens/home_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Movie App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
 
