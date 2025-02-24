349c9970ed805196644da292c894704c4b4c6e76
meguerhi1 <meguerhi1@gmail.com>
2025-02-24 21:44:27 +0100

    Update movie_list.dart

--- movie_list.dart
+++ movie_list.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:movie_app/services/api_service.dart';
 
 class MovieList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-      stream: FirebaseFirestore.instance.collection('movies').snapshots(),
+    return FutureBuilder<List<dynamic>>(
+      future: ApiService.getMovies(),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return Center(child: CircularProgressIndicator());
@@ -14,55 +13,21 @@ class MovieList extends StatelessWidget {
         if (snapshot.hasError) {
           return Center(child: Text('حدث خطأ ما!'));
         }
-        final movies = snapshot.data!.docs;
+        final movies = snapshot.data!;
         return ListView.builder(
           shrinkWrap: true,
           physics: NeverScrollableScrollPhysics(),
           itemCount: movies.length,
           itemBuilder: (context, index) {
-            final movie = movies[index].data() as Map<String, dynamic>;
+            final movie = movies[index];
             return Card(
               margin: EdgeInsets.all(8),
               child: ListTile(
-                leading: ClipRRect(
-                  borderRadius: BorderRadius.circular(8),
-                  child: Image.network(
-                    movie['poster_url'],
-                    width: 50,
-                    height: 80,
-                    fit: BoxFit.cover,
-                  ),
-                ),
+                leading: Image.network(movie['poster_url']),
                 title: Text(movie['title']),
-                subtitle: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(movie['description']),
-                    SizedBox(height: 4),
-                    RatingBar.builder(
-                      initialRating: movie['rating'],
-                      minRating: 1,
-                      direction: Axis.horizontal,
-                      allowHalfRating: true,
-                      itemCount: 5,
-                      itemSize: 20,
-                      itemBuilder: (context, _) => Icon(
-                        Icons.star,
-                        color: Colors.amber,
-                      ),
-                      onRatingUpdate: (rating) {
-                        // تحديث التقييم في قاعدة البيانات
-                      },
-                    ),
-                  ],
-                ),
+                subtitle: Text(movie['description']),
                 onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => MovieDetailScreen(movie: movie),
-                    ),
-                  );
+                  // الانتقال إلى صفحة تفاصيل الفيلم
                 },
               ),
             );
